40fbd97c61fd264ec995dc16246dd81ebb465ce8
yajaneya <janeya@yandex.ru>
2022-02-20 11:48:51 +0300

    Перенос констант WebClient в application.yaml

--- core-service/src/main/java/ru/yajaneya/Spring2Geekbrains/core/configs/AppConfig.java
+++ core-service/src/main/java/ru/yajaneya/Spring2Geekbrains/core/configs/AppConfig.java
@@ -3,20 +3,26 @@ package ru.yajaneya.Spring2Geekbrains.core.configs;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
+import ru.yajaneya.Spring2Geekbrains.core.properties.CartServiceIntegrationProperties;
 
 import java.util.concurrent.TimeUnit;
 
 @Configuration
+@EnableConfigurationProperties(
+        CartServiceIntegrationProperties.class
+)
+@RequiredArgsConstructor
 public class AppConfig {
-    @Value("${integrations.cart-service.url}")
-    private String cartServiceUrl;
+    private final CartServiceIntegrationProperties cartServiceIntegrationProperties;
 
     @Value("${integrations.recom-service.url}")
     private String recomServiceUrl;
@@ -25,15 +31,21 @@ public class AppConfig {
     public WebClient cartServiceWebClient() {
         TcpClient tcpClient = TcpClient
                 .create()
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2000)
+                .option(
+                        ChannelOption.CONNECT_TIMEOUT_MILLIS,
+                        cartServiceIntegrationProperties.getConnectTimeout())
                 .doOnConnected(connection -> {
-                    connection.addHandlerLast(new ReadTimeoutHandler(10000, TimeUnit.MILLISECONDS));
-                    connection.addHandlerLast(new WriteTimeoutHandler(2000, TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new ReadTimeoutHandler(
+                            cartServiceIntegrationProperties.getReadTimeout(),
+                            TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(
+                            cartServiceIntegrationProperties.getWriteTimeout(),
+                            TimeUnit.MILLISECONDS));
                 });
 
         return WebClient
                 .builder()
-                .baseUrl(cartServiceUrl)
+                .baseUrl(cartServiceIntegrationProperties.getUrl())
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
                 .build();
     }
---
+++ core-service/src/main/java/ru/yajaneya/Spring2Geekbrains/core/properties/CartServiceIntegrationProperties.java
@@ -0,0 +1,17 @@
+package ru.yajaneya.Spring2Geekbrains.core.properties;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "integrations.cart-service")
+@Data
+@NoArgsConstructor
+public class CartServiceIntegrationProperties {
+    private String url;
+    private Integer connectTimeout;
+    private Integer readTimeout;
+    private Integer writeTimeout;
+}
